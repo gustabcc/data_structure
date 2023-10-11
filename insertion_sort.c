dd2dc4907f1a660cc093c799fb47ca8fa37cce88
Gustavo Henrique <gustavo_hmoura@outlook.com>
2023-10-11 14:40:58 -0300

    insertion sort implementation

---
+++ insertion_sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h> 
+
+void insertion_sort(int arr[], int n) {
+    int j, k, key;
+
+    for(int i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Array original: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    insertion_sort(arr, n);
+
+    printf("\nArray ordenado: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
